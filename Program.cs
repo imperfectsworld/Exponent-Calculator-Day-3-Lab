bcbbe5ad3efbfabf7432959a5bff4abfd098d72a
imperfectsworld <imperfectsworld@gmail.com>
2024-08-07 17:13:04 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+
+bool restart = true;
+
+
+Console.WriteLine("Time to learn your squares and cubes!");
+
+do
+{
+    restart = StartProgram();
+}
+while (restart == true);
+
+
+// method Start the Program
+
+static bool StartProgram()
+{
+    bool validate = true;
+    int usernum;
+
+    do
+    {
+        Console.WriteLine("Enter an integer: ");
+        usernum = int.Parse(Console.ReadLine());
+
+        {
+            if (usernum <= 0 || (usernum) > Math.Cbrt(int.MaxValue) )  // makes it so that the code doesnt overflow
+            {
+                Console.WriteLine("Incorrect Try Again");
+                
+                validate = true;
+                
+
+            }
+            else
+            {
+                validate = false;
+            }
+
+        }
+
+
+    }
+    while (validate == true);
+
+   
+
+
+  
+
+    Console.WriteLine("Number\t\tSquared\t\tCubed");
+    Console.WriteLine("=======\t\t======\t\t======");
+    for(int i = 1; i <= usernum; i++)
+    {
+        
+        int square = i * i;
+        int cube = i * i * i;
+        Console.WriteLine($"{i}\t\t{square}\t\t{cube}");
+        
+        
+    }
+
+
+
+    Console.WriteLine("Would you like to try again y/n?");
+    string tryAgain = Console.ReadLine();
+
+
+    if (tryAgain == "y" || tryAgain == "Y")
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+
+
+
+
+}
